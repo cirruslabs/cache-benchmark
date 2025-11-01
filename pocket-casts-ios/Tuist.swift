5b4b8e507f768749b8e22f94454d3ea711a6a40f
fortmarek <marekfort@me.com>
2025-11-01 16:37:27 +0100

    Temporarily update full handle of pocket casts

--- pocket-casts-ios/Tuist.swift
+++ pocket-casts-ios/Tuist.swift
@@ -1,7 +1,7 @@
 import ProjectDescription
 
 let tuist = Tuist(
-    fullHandle: "tuist/pocket-casts",
+    fullHandle: "tuist/pocket-casts-two",
     url: "https://staging.tuist.dev",
     project: .xcode()
 )
