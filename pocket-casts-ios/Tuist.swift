bd7d62fcadfb0456373d55ea8bd869e88ba41bd4
fortmarek <marekfort@me.com>
2025-11-04 11:55:41 +0100

    Revert "Temporarily update full handle of pocket casts"
    This reverts commit 5b4b8e507f768749b8e22f94454d3ea711a6a40f.

--- pocket-casts-ios/Tuist.swift
+++ pocket-casts-ios/Tuist.swift
@@ -1,7 +1,7 @@
 import ProjectDescription
 
 let tuist = Tuist(
-    fullHandle: "tuist/pocket-casts-two",
+    fullHandle: "tuist/pocket-casts",
     url: "https://staging.tuist.dev",
     project: .xcode()
 )
